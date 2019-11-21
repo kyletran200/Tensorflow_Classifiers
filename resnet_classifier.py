767eb4933a648c4183f302b188634b8fd4fdf06a
Kyle <kylevtran201@gmail.com>
2019-11-21 15:13:23 -0800

    78% accuracy with VGG classifier and updated ResNet Classifier.

---
+++ resnet_classifier.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+
+from tensorflow.keras import datasets, models, layers, backend
+
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+
+from tensorflow.keras import Model
+
+import matplotlib.pyplot as plt
+
+import os
