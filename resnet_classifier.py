6d893d700c7e00388441aab577489aa773fffee8
Kyle <kylevtran201@gmail.com>
2019-11-22 12:54:22 -0800

    Update model

--- resnet_classifier.py
+++ resnet_classifier.py
@@ -12,3 +12,30 @@ from tensorflow.keras import Model
 import matplotlib.pyplot as plt
 
 import os
+
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+models = models.Sequential()
+
+
+
+
+
+model.summary()
+
+# Compile our model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, batch_size=10, epochs=10, 
+                    validation_data=(test_images, test_labels))
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print("This is test accuracy: " + str(test_acc))
